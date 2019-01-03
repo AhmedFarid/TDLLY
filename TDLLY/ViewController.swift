f20a9acc97c24a4dedf5f17c755ea4b80b59e6a3
farido <farido@faridos-Mac.local>
2019-01-03 10:37:39 -0800

    Initial Commit

---
+++ TDLLY/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TDLLY
+//
+//  Created by farido on 1/3/19.
+//  Copyright © 2019 Farido. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
